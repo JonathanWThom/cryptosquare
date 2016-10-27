8a8147a5bad0943c853b921904d0c53618a5ad3d
jonathan thom and suzi rubino <suzi@>
2016-10-27 14:56:24 -0700

    Remove spaces, make string lowercase, determine string, and square root of string length

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 // business
+function coded(input) {
+  var noSpaces = input.replace(/\s+/g, "");
+  noSpaces = noSpaces.toLowerCase();
+  var length = noSpaces.length;
+  var squareRoot = Math.sqrt(length);
 
+  if (squareRoot % 1 != 0) {
+    console.log("decimals")
+  } else {
+    console.log("evenly divided")
+  }
+}
 
 
 // user interface
