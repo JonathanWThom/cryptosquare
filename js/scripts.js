058be8d81fd68533e85d522aabd48f34bbb69fb8
jonathan thom and suzi rubino <jonathan@>
2016-10-27 16:24:59 -0700

    Sort letters in proper array

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,26 @@ function coded(input) {
   noSpaces = noSpaces.toLowerCase();
   var length = noSpaces.length;
   var squareRoot = Math.sqrt(length);
+  console.log(length);
+
 
   if (squareRoot % 1 != 0) {
     var column = (Math.floor(squareRoot) + 1)
-    console.log(column);
   } else {
     var column = squareRoot;
-    console.log(column);
   }
+
+  console.log(column);
+
+  var x = 0;
+  for (index = 0; index < column; index++) {
+    for (i = x; i < length; i += column) {
+      var encrypted = noSpaces.charAt(i);
+      console.log(encrypted);
+      }
+    x++;
+    }
 }
-
-
 // user interface
 $(document).ready(function(){
   $("#encryption").submit(function(event) {
